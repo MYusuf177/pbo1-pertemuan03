9668ce225fb6c2ceb22eb03d9e84686e0968a9f4
MYusuf177 <yusufmedia123@gmail.com>
2020-05-01 12:35:22 +0800

    Operator Binary Assignment

---
+++ src/Operator/OperatorBinary.java
@@ -0,0 +1,14 @@
+package Operator;
+
+public class OperatorBinary {
+    public static void main(String[] args) {
+
+        //operator (=) sebagai assignment/penugasan
+        //menempatkan nilai kedalam variabel
+        //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+        String nama = "M. Yusuf";
+        int jumlahSks = 24;
+        double indeksPrestasiKumulatif = 3.89;
+    }
+}
